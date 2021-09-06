5ff3683a187b0c37f417c2f3e515d226ed7b600d
John <john.sarmiento452@gmail.com>
2021-09-06 10:55:57 +0800

    auth0 setup

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
-
+//dev-jhy-48ff.us.auth0.com
+//mSKFkdLWPuwYTSPVUKhBTdXKjwncaqyT
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-        <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-jhy-48ff.us.auth0.com"
+      clientId="mSKFkdLWPuwYTSPVUKhBTdXKjwncaqyT"
+      redirectUri={window.location.origin}
+    > 
+      <GithubProvider>
+          <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
